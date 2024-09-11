8c35ee52fa122cd039213b1b95dc64b019f75372
Rahul Mahato <mahatorahul025@gmail.com>
2024-09-11 21:03:34 +0530

    completed post controller

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import serverConfig from "./src/config/server.config.js";
 import apiRouter from "./src/routes/index.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api", apiRouter);
 
